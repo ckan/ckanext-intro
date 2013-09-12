0e60787c7b22c689c572e331e6d6902561c2f99e
amercader <amercadero@gmail.com>
2013-09-12 17:08:48 +0100

    Step 1: Initial structure
    Nothing has happened yet. We created the extension with the `paster
    create` command as described here:
    
    http://docs.ckan.org/en/latest/extensions/tutorial.html#creating-a-new-extension
    
    At this point we only have a `ckanext/intro` folder, where all the
    extension files will go, and a `setup.py` file, which contains:
    
    * Metadata about your extension (you filled some of those when running
      the `paster create` command, and you can update them now or later).
    
    * Settings for installing your extension in a virtualenv. Don't worry
      about these for now.
    
    * An `entry_points` setting with a [ckan.plugins] section. This is an
      important bit. It is where you will add your extension plugins in
      order to register them, as we will see on the next step.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(
+	name='ckanext-intro',
+	version=version,
+	description="Quick introduction to writiing CKAN extensions",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Adri\xc3\xa0 Mercader',
+	author_email='',
+	url='http://ckan.org',
+	license='GPL v3.0',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.intro'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.intro:PluginClass
+	""",
+)
