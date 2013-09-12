1c208823ca09c5f3fe9ab615858a636f4242618a
amercader <amercadero@gmail.com>
2013-09-12 18:18:45 +0100

    Step 3: Implement an interface
    Plugins can interact with CKAN by implementing one of its *interfaces*.
    These provide methods which are hooks into the internal CKAN logic,
    allowing plugins to customize the default behaviour. For a full list of
    the available interfaces, check
    
    http://docs.ckan.org/en/latest/extensions/plugin-interfaces.html
    
    or the source of the `ckan/plugins/interfaces.py` file.
    
    To implement an Interface, plugins need to call the `implements` method
    with the interface class as a parameter.
    
    In our particular case, we are implementing the `IConfigurer` interface,
    which allows to access and modify the CKAN config object. We are not
    really doing yet, other than adding a log message with the currently
    used plugins.
    
    The `IConfigurer` interface is really useful because it is called right
    at the start of the CKAN initialization, so you if your extension needs
    to perform some action when CKAN is started, this is the method to use.

--- ckanext/intro/plugin.py
+++ ckanext/intro/plugin.py
@@ -1,5 +1,20 @@
+import logging
+
 import ckan.plugins as p
 
 
+log = logging.getLogger(__name__)
+
+
 class IntroExamplePlugin(p.SingletonPlugin):
-    pass
+
+    p.implements(p.IConfigurer)
+
+    ## IConfigurer
+    def update_config(self, config):
+        '''
+        This method allows to access and modify the CKAN configuration object
+        '''
+
+        log.info('You are using the following plugins: {0}'
+                 .format(config.get('ckan.plugins')))
