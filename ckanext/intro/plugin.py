0415777c2c49be28949a9423c5c359727c36605a
amercader <amercadero@gmail.com>
2013-09-13 18:09:00 +0100

    Step 9: Action and Auhtorization functions 2: custom actions
    On the last step we saw what action and authorization functions are and how you
    register them from your plugin.
    
    On this step, we added a new route (see Step 7), `/csv`, that when visited
    outputs a CSV file with a report of all the datasets present on your CKAN
    instance. The route points to a new method on our small controller, which in
    turn calls an action function called `datasets_report_csv`.
    
    This action function is defined on our `plugin.py` file, and we have registered
    it using the `get_actions` method of the `IActions` interface. As with all
    action and auth functions, receives a context and data_dict objects, although
    in our case we won't use them.
    
    The first thing that the action function does is check if the user is
    authorized, this is done calling `toolkit.check_access` with the name of the
    registered auth function. If one of your action functions needs the same
    authorization as an existing action function you can of course call it as well.
    For instance if you want to allow users to do some action on a dataset only if
    they can edit it, you would call:
    
        p.toolkit.check_access('package_update', context, data_dict)
    
    Note how our controller captures the `toolkit.NotAuhtorized` exceptions raised
    by the authorization functions and aborts the request with a 401 status.
    
    If the user is authorized (ie on our case is a sysadmin) we then call the
    `package_search` action to get details of all datasets, and use the python
    `csv` module to create a CSV file. The path of this file is passed to the
    controller, which will read it and output it.
    
    Note how we modify the headers of the response to reflect that we are outputing
    a CSV file.
    
    To request the dataset report, we added a link to the footer as well.

--- ckanext/intro/plugin.py
+++ ckanext/intro/plugin.py
@@ -1,4 +1,7 @@
 import logging
+import os
+import csv
+import tempfile
 
 import ckan.plugins as p
 
@@ -11,6 +14,7 @@ class IntroExamplePlugin(p.SingletonPlugin):
     p.implements(p.IConfigurer)
     p.implements(p.IRoutes, inherit=True)
     p.implements(p.IAuthFunctions)
+    p.implements(p.IActions)
 
     ## IConfigurer
     def update_config(self, config):
@@ -40,14 +44,25 @@ class IntroExamplePlugin(p.SingletonPlugin):
         controller = 'ckanext.intro.plugin:CustomController'
         map.connect('/custom', controller=controller, action='custom_page')
 
+        map.connect('/csv', controller=controller, action='datasets_report')
+
         return map
 
     ## IAuthFunctions
     def get_auth_functions(self):
 
         # Return a dict with the auth functions that we want to override
+        # or add
         return {
             'group_create': group_create,
+            'datasets_report_csv': datasets_report_csv_auth,
+        }
+
+    ## IActions
+    def get_actions(self):
+        # Return a dict with the action functions that we want to add
+        return {
+            'datasets_report_csv': datasets_report_csv,
         }
 
 
@@ -59,8 +74,85 @@ def group_create(context, data_dict):
     return {'success': False, 'msg': 'Only sysadmins can create groups'}
 
 
+# Ideally auth functions should have the same name as their actions and be
+# kept on different modules. To keep things simple and all the functions on
+# the same file we will name this one slightly different.
+def datasets_report_csv_auth(context, data_dict):
+
+    return {'success': False, 'msg': 'Only sysadmins can get a report'}
+
+
+def datasets_report_csv(context, data_dict):
+    '''
+    A custom action function that generates a CSV file with metadata from
+    all datasets in the CKAN instance and stores it on a temporal file,
+    returning its path.
+
+    Note how we call `p.toolkit.check_access` to make sure that the user is
+    authorized to perform this action.
+    '''
+
+    p.toolkit.check_access('datasets_report_csv', context, data_dict)
+
+    # Get all datasets from the search index (actually the first 100)
+    data_dict = {
+        'q': '*:*',
+        'rows': 100,
+    }
+    result = p.toolkit.get_action('package_search')(context, data_dict)
+
+    # Create a temp file to store the csv
+    fd, tmp_file_path = tempfile.mkstemp(suffix='.csv')
+
+    with open(tmp_file_path, 'w') as f:
+        field_names = ['name', 'title', 'tags',
+                       'metadata_created', 'metadata_modified']
+        writer = csv.DictWriter(f, fieldnames=field_names,
+                                quoting=csv.QUOTE_ALL)
+        writer.writerow(dict((n, n) for n in field_names))
+        for dataset in result['results']:
+            row = {}
+            for field_name in field_names:
+                if field_name == 'tags':
+                    tag_names = [t['name'] for t in dataset.get('tags', [])]
+                    row['tags'] = ', '.join(tag_names).encode('utf8')
+                else:
+                    row[field_name] = dataset[field_name].encode('utf8')
+            writer.writerow(row)
+
+        return {
+            'file': tmp_file_path,
+        }
+
+
 class CustomController(p.toolkit.BaseController):
 
     def custom_page(self):
 
         return p.toolkit.render('custom.html')
+
+    def datasets_report(self):
+
+        try:
+            # We don't need to pass any parameters to the action function
+            # (context and data_dict), as a new context will be automatically
+            # created for you, and we don't need to pass any data from the
+            # user to the action function.
+            result = p.toolkit.get_action('datasets_report_csv')()
+        except p.toolkit.NotAuthorized:
+            p.toolkit.abort(401, 'Not authorized to see this report')
+
+        with open(result['file'], 'r') as f:
+            content = f.read()
+
+        # Clean up
+        os.remove(result['file'])
+
+        # Modify the headers of the response to reflect that we are outputing
+        # a CSV file
+        p.toolkit.response.headers['Content-Type'] = 'application/csv'
+        p.toolkit.response.headers['Content-Length'] = len(content)
+        p.toolkit.response.headers['Content-Disposition'] = \
+            'attachment; filename="datasets.csv"'
+
+        return content
